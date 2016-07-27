149ca1a9f7870d76f32d5ab3667b86b6583ca41c
peters313 <eric.peters3@gmail.com>
2016-07-26 19:37:28 -0500

    attempting to import native-base into the project

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+import React, { Component } from 'react';
+import { list of Components } from 'native-base';
+import { Container, Content, Text } from 'native-base';
+
+<Header searchBar rounded>
+    <InputGroup>
+        <Icon name="ios-search" />
+        <Input placeholder="Search" value={this.state.search}  onChangeText={(text) => this.setState({search:text})} onSubmitEditing={()=>this.search()}/>
+    </InputGroup>
+    <Button transparent onPress={()=>this.search()}>Go</Button>
+</Header>
+
+search() {
+    // Set loading to true when the search starts to display a Spinner
+    this.setState({
+        loading: true
+    });
+
+    var that = this;
+    return fetch('https://api.github.com/search/repositories?q='+this.state.search)
+    .then((response) => response.json())
+    .then((responseJson) => {
+        // Store the results in the state variable results and set loading to                  // false to remove the spinner and display the list of repositories                 that.setState({
+        results: responseJson,
+        loading: false
+    });
+    return responseJson.Search;
+})
+    .catch((error) => {
+        that.setState({
+        loading: false
+    });
+        console.error(error);
+    });
+}
--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,57 @@
-'use strict'
-import React, { Component } from 'react';
-import {AppRegistry, StyleSheet, Text, View } from 'react-native';
-import textInput from './app/components/textInput'
+import App from './app';
 
-class littertracker extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {text: ''};
-}
-  render() {
+export default App;
 
-    return (
-      <View style={{padding: 10}}>
-              <TextInput
-                style={{height: 40}}
-                placeholder="Type here to translate!"
-                onChangeText={(text) => this.setState({text})}
-              />
-              <Text style={{padding: 10, fontSize: 42}}>
-                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-              </Text>
-            </View>
-        <View>
-        <Text style={styles.welcome}>Welcome to your Litter Tracker!
-        </Text>
-        </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 15,
-    marginTop: 50,
-    backgroundColor: 'skyblue'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('littertracker', () => littertracker);
+// 'use strict'
+// import React, { Component } from 'react';
+// import {AppRegistry, StyleSheet, Text, View } from 'react-native';
+// import textInput from './app/components/textInput'
+//
+// class littertracker extends Component {
+//   constructor(props) {
+//   super(props);
+//   this.state = {text: ''};
+// }
+//   render() {
+//
+//     return (
+//       <View style={{padding: 10}}>
+//               <TextInput
+//                 style={{height: 40}}
+//                 placeholder="Type here to translate!"
+//                 onChangeText={(text) => this.setState({text})}
+//               />
+//               <Text style={{padding: 10, fontSize: 42}}>
+//                 {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+//               </Text>
+//             </View>
+//         <View>
+//         <Text style={styles.welcome}>Welcome to your Litter Tracker!
+//         </Text>
+//         </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 15,
+//     marginTop: 50,
+//     backgroundColor: 'skyblue'
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+//
+// AppRegistry.registerComponent('littertracker', () => littertracker);
