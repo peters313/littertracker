d4b69fc825b5c8e22b5d33af7af984d92cc0f75a
peters313 <eric.peters3@gmail.com>
2016-08-03 20:16:08 -0500

    starts header component

---
+++ Header.js
@@ -0,0 +1,16 @@
+'use strict'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class Header extends Component {
+  render(){
+    return(
+      <View>
+      <Text>HEADER</Text>
+      </View>
+    )
+  }
+}
+
+module.exports = Header;
--- index.ios.js
+++ index.ios.js
@@ -2,11 +2,13 @@
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 var Login = require('./Login');
+var Header = require('./Header');
 
 class littertracker extends Component {
   render() {
     return (
         <View>
+        <Header />
         <Login />
         </View>
     );
