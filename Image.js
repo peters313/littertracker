008b999922dfe60ddaa033f89dcc1c1a8f0c31be
peters313 <eric.peters3@gmail.com>
2016-08-08 22:29:19 -0500

    refactoring

--- Image.js
+++ Image.js
@@ -1,7 +1,7 @@
 'use strict'
 import React, { Component } from 'react';
-import {View, Image} from 'react-native';
-var backgroundImg = require('./mainImage.jpg');
+import { View, Image } from 'react-native';
+var backgroundImg = require('./mainImage.png');
 
 
 class mainImage extends Component {
--- index.ios.js
+++ index.ios.js
@@ -4,14 +4,12 @@ import {AppRegistry, StyleSheet, Text, Image, View} from 'react-native';
 var Login = require('./Login');
 var Header = require('./Header');
 var Footer = require('./Footer');
-var mainImage = require('./Image');
 
 class littertracker extends Component {
   render() {
     return (
         <View>
         <Header />
-        <mainImage />
         <Login />
         <Footer />
         </View>
